bb99868a5e925468740557239e31c76d779cbefd
Tahsin Kabir <tahsinkabir@gmail.com>
2025-05-25 16:08:14 -0400

    Restore UI fixes and README updates - Reverted UI files to original 2010 state for better compatibility

--- src/assembler/PipelineFrame.java
+++ src/assembler/PipelineFrame.java
@@ -1,32 +1,27 @@
 package assembler;
 
-
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author 'SIN
  */
-public class PipelineFrame extends JFrame{
+public class PipelineFrame extends JFrame {
 
     PipelinePanel pp;
 
-    PipelineFrame(String title,String ins)
-    {
+    PipelineFrame(String title, String ins) {
         super(title);
         pp = new PipelinePanel(ins);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(pp.getWidth()+10,pp.getHeight() + 10);
+        this.setSize(pp.getWidth() + 10, pp.getHeight() + 10);
         this.setVisible(true);
         this.add(pp);
-        this.setBounds(100,50,this.getWidth(),this.getHeight());
+        this.setBounds(100, 50, this.getWidth(), this.getHeight());
         this.setResizable(false);
     }
 }
